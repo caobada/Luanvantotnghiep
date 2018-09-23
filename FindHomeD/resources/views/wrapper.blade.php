43a5921f0675afc584afcbca9a0da8506d2b3cc9
DESKTOP-2PO5H4F\caoba <cao.bada96@gmail.com>
2018-09-23 11:15:42 +0700

    update source

--- FindHomeD/resources/views/wrapper.blade.php
+++ FindHomeD/resources/views/wrapper.blade.php
@@ -11,6 +11,8 @@
     <title>Portfolio Item - Start Bootstrap Template</title>
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
 
     <!-- Custom styles for this template -->
     <link href="{{asset('css/portfolio-item.css')}}"   rel="stylesheet">
@@ -21,8 +23,9 @@
 
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">FindHomeD</a>
+        <a class="navbar-brand" href="#">
+          <img src="https://uploads-ssl.webflow.com/5b860983eac5c70d96636e4d/5b9213e7ae89e5416ad5d6fa_logo.svg" width="200" >
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -34,15 +37,15 @@
               </a>
             </li>
             <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Loại cho thuê
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          @foreach($type_of_rental as $data)
-          <a class="dropdown-item" href="#">{{$data->nametype}}</a>
-          @endforeach
-        </div>
-           </li>
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Loại cho thuê
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                @foreach($type_of_rental as $data)
+                <a class="dropdown-item" href="#">{{$data->nametype}}</a>
+                @endforeach
+              </div>
+            </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Đăng Tin</a>
             </li>
@@ -50,12 +53,67 @@
               <a class="nav-link" href="#">Liên Hệ</a>
             </li>
           </ul>
+          <ul class="navbar-nav ml-right">
+            <li class="nav-item">
+              <a href="#">Đăng Nhập</a>
+            </li>
+            <li class="nav-item create-acc">
+              <a href="#">Đăng kí tài khoản</a>
+            </li>  
+          </ul>
         </div>
-      </div>
     </nav>
-
+<!--  -->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://uploads-ssl.webflow.com/5b860983eac5c70d96636e4d/5b8632bdeac5c7d4d4638af0_kids-1093758_1920.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://uploads-ssl.webflow.com/5b860983eac5c70d96636e4d/5b8638722532c3734f5a988d_children-602967_1920.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://uploads-ssl.webflow.com/5b860983eac5c70d96636e4d/5b8632bdf8740394b4932bfa_kids-2985782_1920.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
     <!-- Page Content -->
     <div class="container">
+      <div class="col-md-12">
+      <a href="#costumModal2" role="button" class=" top-search" data-toggle="modal"><i class="fa fa-search"></i> TÌM KIẾM NGAY</a>
+      </div>
+        <div id="costumModal2" class="modal" data-easein="flipXIn"  tabindex="-1" role="dialog" aria-labelledby="costumModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">
+                            Tìm kiếm nhanh
+                        </h4>
+                    </div>
+                    <div class="modal-body">
+              
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-primary">
+                            Tìm nhanh
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
       <!-- Portfolio Item Heading -->
       <h1 class="my-4">Top Tin
         /<small>HOT</small>
@@ -157,6 +215,12 @@
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/bootstrap/js/index.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.2/velocity.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.2/velocity.ui.min.js'></script>
     <script src="js/stype.js"></script>
+    <script>
+    $("#carouselExampleControls").carousel();
+      </script>
   </body>
 </html>
